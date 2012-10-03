07363189ef9056e953d2ae4702a3a0e8c5f5468d
unknown <tmw@EAS-SJCA-008.(none)>
2012-10-03 08:39:39 -0700

    Added views

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-var express = require('express');
+var express = require ('express');
+var mysql = require('mysql');
+var config = require('./config');
+  
+app = express.createServer();
+app.set('view engine', 'jade');
+app.set('views',__dirname + '/views');
+app.set('view options', { layout: false });
 
-var app = express.createServer(express.logger());
+var db = mysql.createConnection(config);
+app.use(express.bodyParser());
 
-app.get('/', function(request, response) {
-  response.send('Hello World new em!');
+// main route
+app.get('/', function (req, res, next) {
+	db.query('SELECT firstname, lastname from student', function (err, results) {
+		res.render('index', { items: results });
+	});
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// item create
+app.post('/create', function (req, res, next) {
+	console.log('create called');
+	var firstname=req.query.firstname;
+	var lastname=req.query.lastname;
+	console.log('Creating: '+firstname+' '+lastname);
+	db.query('INSERT INTO student SET firstname = ?, lastname = ?',
+	[firstname, lastname], function (err, info) {
+		if (err) return next(err);
+		console.log('item created with id %s', info.insertID);
+		res.send('item created with id %s', info.insertID);
+	});
+});
+
+//item route
+app.get('/item/:id', function(req, res, next) {
+	
+});
+
+// listen
+app.listen(3000, function () {
+	console.log(' - listening on http://*:3000');
 });
